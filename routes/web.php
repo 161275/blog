8c8e5f20625e833ce4d66e914956f6482287f2f8
161275 <nishchalgarg123@gmail.com>
2017-11-06 19:23:04 +0530

    FIXED:saperate controllers for all the pages

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('user/blog');
+//user Routes
+Route::group(['namespace'=>'User'],function()
+{
+Route::get('/', 'Homecontroller@index');
+Route::get('post', 'Postcontroller@index')->name('post');
 });
-Route::get('post', function () {
-    return view('user/post');
-})->name('post');
-Route::get('admin/home', function (){
-    return view('admin/home');
-});
-Route::get('admin/post', function (){
-    return view('admin.post.post');
-});
-Route::get('admin/category', function (){
-    return view('admin.category.category');
-});
-Route::get('admin/tag', function (){
-    return view('admin.tag.tag');
+//Admin Routes
+Route::group(['namespace'=>'admin'],function()
+{
+Route::get('admin/home', 'Homecontrolle@index')->name('admin.home');
+Route::resource('admin/user','usercontroller');
+Route::resource('admin/post','postcontroller');
+Route::resource('admin/tag','tagcontroller');
+Route::resource('admin/category','categorycontroller');
 });
